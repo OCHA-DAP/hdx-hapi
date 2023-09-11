a59663f5c75ecc38c012f20602766321cd221af2
Catalin <catalinflorentinion@gmail.com>
2023-09-11 12:04:54 +0300

    admin1 endpoint

---
+++ hdx_hapi/db/dao/admin1_view_dao.py
@@ -0,0 +1,40 @@
+from typing import Dict
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from hdx_hapi.db.models.views.db_admin1_view import Admin1View
+from hdx_hapi.db.dao.util.util import apply_pagination
+
+from datetime import datetime
+import dateutil.parser
+
+async def admin1_view_list(
+    pagination_parameters: Dict,
+    db: AsyncSession,
+    code: str = None,
+    name: str = None,
+    is_unspecified: bool = None,
+    reference_period_start: datetime = None,
+    reference_period_end: datetime = None,
+):
+
+    query = select(Admin1View)
+    if code:
+        query = query.where(Admin1View.code == code)
+    if name:
+        query = query.where(Admin1View.name.icontains(name))
+    if is_unspecified:
+        query = query.where(Admin1View.is_unspecified == is_unspecified)
+    if reference_period_start:
+        start_date = dateutil.parser.parse(reference_period_start)
+        query = query.where(Admin1View.reference_period_start >= start_date)
+    if reference_period_end:
+        end_date = dateutil.parser.parse(reference_period_end)
+        query = query.where(Admin1View.reference_period_end <= end_date)
+
+    query = apply_pagination(query, pagination_parameters)
+
+    result = await db.execute(query)
+    locations = result.scalars().all()
+    return locations
---
+++ hdx_hapi/endpoints/get_admin1.py
@@ -0,0 +1,35 @@
+from typing import List, Annotated, Dict
+from fastapi import Depends, Query, APIRouter
+
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from hdx_hapi.endpoints.models.admin1_view import Admin1ViewPydantic
+from hdx_hapi.endpoints.util.util import pagination_parameters
+from hdx_hapi.services.admin1_logic import get_admin1_srv
+from hdx_hapi.services.sql_alchemy_session import get_db
+
+router = APIRouter(
+    tags=['admin1'],
+)
+
+@router.get('/api/admin1', response_model=List[Admin1ViewPydantic])
+async def get_admin1(
+    pagination_parameters: Annotated[dict, Depends(pagination_parameters)],
+    db: AsyncSession = Depends(get_db),
+    code: Annotated[str, Query(max_length=10)] = None,
+    name: Annotated[str, Query(max_length=10)] = None,
+    is_unspecified: Annotated[bool, Query()] = None,
+    reference_period_start: Annotated[str, Query(len=19)] = None,
+    reference_period_end: Annotated[str, Query(len=19)] = None,
+):
+    result = await get_admin1_srv(
+        pagination_parameters=pagination_parameters,
+        db=db,
+        code=code,
+        name=name,
+        is_unspecified=is_unspecified,
+        reference_period_start=reference_period_start,
+        reference_period_end=reference_period_end,
+    )
+    return result
