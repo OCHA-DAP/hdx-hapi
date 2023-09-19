da124090ee928ac2c6f2ef5ea542854be25d0a46
Catalin <catalinflorentinion@gmail.com>
2023-09-19 12:28:49 +0000

    add tests

--- hdx_hapi/db/models/views/db_org_view.py
+++ hdx_hapi/db/models/views/db_org_view.py
@@ -8,7 +8,7 @@ class OrgView(Base):
     __tablename__ = 'org_view'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    hdx_link: Mapped[str] = mapped_column(String(1024), nullable=False)
+    # hdx_link: Mapped[str] = mapped_column(String(1024), nullable=False)
     acronym: Mapped[str] = mapped_column(String(32), nullable=False, index=True)
     name: Mapped[str] = mapped_column(String(512), nullable=False)
     org_type_code: Mapped[str] = mapped_column(ForeignKey('org_type.code', onupdate='CASCADE', ondelete='CASCADE'))
