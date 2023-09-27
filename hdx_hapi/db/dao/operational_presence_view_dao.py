71271110d7362ced1b6f1dd8c1a70afc66e9084d
Dan Mihaila <danmihaila@gmail.com>
2023-09-27 14:52:23 +0300

    HAPI-49 OP updated fields without dates

--- hdx_hapi/db/dao/operational_presence_view_dao.py
+++ hdx_hapi/db/dao/operational_presence_view_dao.py
@@ -14,49 +14,40 @@ logger = logging.getLogger(__name__)
 async def operational_presences_view_list(
     pagination_parameters: Dict,
     db: AsyncSession,
-    org_ref: int = None,
-    dataset_hdx_id: str = None,
-    dataset_hdx_stub: str = None,
-    sector_name: str = None,
+    sector_code: int = None,
     dataset_provider_code: str = None,
     org_acronym: str = None,
     org_name: str = None,
+    sector_name: str = None,
     location_code: str = None,
     location_name: str = None,
     admin1_code: str = None,
-    admin1_name: str = None,
+    admin1_is_unspecified: bool = None,
     admin2_code: str = None,
     admin2_name: str = None,
-    admin1_is_unspecified: bool = None,
     admin2_is_unspecified: bool = None,
 
 ):
 
-    logger.info(f'operational_presences_view_list called with params: org_ref={org_ref}, location_name={location_name}')
+    # logger.info(f'operational_presences_view_list called with params: org_ref={org_ref}, location_name={location_name}')
 
     query = select(OperationalPresenceView)
-    if org_ref:
-        query = query.where(OperationalPresenceView.org_ref == org_ref)
-    if sector_name:
-        query = query.where(OperationalPresenceView.sector_name == sector_name)
-    if dataset_hdx_id:
-        query = query.where(OperationalPresenceView.dataset_hdx_id == dataset_hdx_id)
-    if dataset_hdx_stub:
-        query = query.where(OperationalPresenceView.dataset_hdx_stub == dataset_hdx_stub)
+    if sector_code:
+        query = query.where(OperationalPresenceView.sector_code == sector_code)
     if dataset_provider_code:
         query = query.where(OperationalPresenceView.dataset_provider_code == dataset_provider_code)
     if org_acronym:
         query = query.where(OperationalPresenceView.org_acronym == org_acronym)
     if org_name:
         query = query.where(OperationalPresenceView.org_name == org_name)
+    if sector_name:
+        query = query.where(OperationalPresenceView.sector_name == sector_name)
     if location_code:
         query = query.where(OperationalPresenceView.location_code == location_code)
     if location_name:
         query = query.where(OperationalPresenceView.location_name == location_name)
     if admin1_code:
         query = query.where(OperationalPresenceView.admin1_code == admin1_code)
-    if admin1_name:
-        query = query.where(OperationalPresenceView.admin1_name == admin1_name)
     if admin2_code:
         query = query.where(OperationalPresenceView.admin2_code == admin2_code)
     if admin2_name:
--- hdx_hapi/endpoints/models/operational_presence_view.py
+++ hdx_hapi/endpoints/models/operational_presence_view.py
@@ -4,20 +4,11 @@ from typing import Optional
 
 class OperationalPresenceViewPydantic(BaseModel):
 
-    org_ref: int = None,
-
-    dataset_hdx_id: str = Field(max_length=36),
+    sector_code: str = Field(max_length=32),
     dataset_hdx_stub: str = Field(max_length=128),
-    dataset_title: str = Field(max_length=1024),
-    dataset_provider_code: str = Field(max_length=128),
-    dataset_provider_name: str = Field(max_length=512),
     resource_hdx_id: str = Field(max_length=36),
-    resource_filename: str = Field(max_length=256),
-
     org_acronym: str = Field(max_length=32),
     org_name: str = Field(max_length=512),
-    org_type_code: str = Field(max_length=32),
-    org_type_description: str = Field(max_length=512),
     sector_name: str = Field(max_length=512),
     location_code: str = Field(max_length=128),
     location_name: str = Field(max_length=512),
@@ -25,6 +16,14 @@ class OperationalPresenceViewPydantic(BaseModel):
     admin1_name: str = Field(max_length=512),
     admin2_code: str = Field(max_length=128),
     admin2_name: str = Field(max_length=512),
+    # org_ref: int = None,
+    # dataset_hdx_id: str = Field(max_length=36),
+    # dataset_title: str = Field(max_length=1024),
+    # dataset_provider_code: str = Field(max_length=128),
+    # dataset_provider_name: str = Field(max_length=512),
+    # resource_filename: str = Field(max_length=256),
+    # org_type_code: str = Field(max_length=32),
+    # org_type_description: str = Field(max_length=512),
 
 
     class Config:
