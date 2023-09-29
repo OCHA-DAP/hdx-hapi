17b34f11acf9bfd38b945860df32a3701accf1ac
Catalin <catalinflorentinion@gmail.com>
2023-09-29 11:36:20 +0000

    HAPI-129 review op params + add more tests

--- tests/test_endpoints/test_operational_presence_endpoint.py
+++ tests/test_endpoints/test_operational_presence_endpoint.py
@@ -1,3 +1,4 @@
+from datetime import date
 import pytest
 import logging
 
@@ -14,4 +15,38 @@ async def test_get_operational_presences(event_loop, refresh_db):
         response = await ac.get('/api/themes/3W')
     assert response.status_code == 200
     assert len(response.json()) > 0, 'There should be at least one operational presence in the database'
-    
+
+
+@pytest.mark.asyncio
+async def test_get_operational_presence_params(event_loop, refresh_db):
+    log.info('started test_get_operational_presence_params')
+
+    params = {
+        'sector_code': 'SHL',
+        'dataset_provider_code': 'provider01',
+        'resource_update_date_min': date(2023, 6, 1),
+        'resource_update_date_max': date(2023, 6, 2),
+        'org_acronym': 'ORG01',
+        'org_name': 'Organisation 1',
+        'sector_name': 'Emergency Shelter and NFI',
+        'location_code': 'FOO',
+        'location_name': 'Foolandia',
+        'admin1_code': 'FOO-001',
+        'admin1_is_unspecified': False,
+        'admin2_code': 'FOO-001-XXX',
+        'admin2_name': 'Unspecified',
+        'admin2_is_unspecified': True,
+    }
+
+    for param_name, param_value in params.items():
+        async with AsyncClient(app=app, base_url="http://test", params={param_name: param_value}) as ac:
+            response = await ac.get('/api/themes/3W')
+
+        assert response.status_code == 200
+        assert len(response.json()) > 0, f'There should be at least one operational_presence entry for parameter "{param_name}" with value "{param_value}" in the database'
+
+    async with AsyncClient(app=app, base_url="http://test", params=params) as ac:
+        response = await ac.get('/api/themes/3W')
+
+    assert response.status_code == 200
+    assert len(response.json()) > 0, f'There should be at least one operational_presence entry for all parameters in the database'
