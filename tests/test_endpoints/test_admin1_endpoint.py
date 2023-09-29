6f5500562bd1f2e5fc4d945afd4b4fe7971e9e81
Catalin <catalinflorentinion@gmail.com>
2023-09-29 13:09:24 +0000

    HAPI-129 add admin1 result test

--- tests/test_endpoints/test_admin1_endpoint.py
+++ tests/test_endpoints/test_admin1_endpoint.py
@@ -1,3 +1,5 @@
+import json
+import os
 import pytest
 import logging
 
@@ -6,36 +8,48 @@ from main import app
 
 log = logging.getLogger(__name__)
 
+ENDPOINT_ROUTER = '/api/admin1'
+with open(os.path.join(os.path.dirname(__file__), 'endpoint_data.json'), 'r') as file:
+    endpoint_data = json.load(file)[ENDPOINT_ROUTER]
+    query_parameters = endpoint_data['query_parameters']
+    expected_fields = endpoint_data['expected_fields']
+
 
 @pytest.mark.asyncio
 async def test_get_admin1(event_loop, refresh_db):
     log.info('started test_get_admin1')
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get('/api/admin1')
+    async with AsyncClient(app=app, base_url='http://test') as ac:
+        response = await ac.get(ENDPOINT_ROUTER)
     assert response.status_code == 200
     assert len(response.json()) > 0, 'There should be at least one admin1 entry in the database'
-    
+
 
 @pytest.mark.asyncio
 async def test_get_admin1_params(event_loop, refresh_db):
     log.info('started test_get_admin1_params')
 
-    params = {
-        'code': 'FOO-001',
-        'name': 'Province 01',
-        'location_code': 'FOO',
-        'location_name': 'Foolandia'
-    }
-
-    for param_name, param_value in params.items():
-        async with AsyncClient(app=app, base_url="http://test", params={param_name: param_value}) as ac:
-            response = await ac.get('/api/admin1')
+    for param_name, param_value in query_parameters.items():
+        async with AsyncClient(app=app, base_url='http://test', params={param_name: param_value}) as ac:
+            response = await ac.get(ENDPOINT_ROUTER)
 
         assert response.status_code == 200
         assert len(response.json()) > 0, f'There should be at least one admin1 entry for parameter "{param_name}" with value "{param_value}" in the database'
 
-    async with AsyncClient(app=app, base_url="http://test", params=params) as ac:
-        response = await ac.get('/api/admin1')
+    async with AsyncClient(app=app, base_url='http://test', params=query_parameters) as ac:
+        response = await ac.get(ENDPOINT_ROUTER)
 
     assert response.status_code == 200
     assert len(response.json()) > 0, f'There should be at least one admin1 entry for all parameters in the database'
+
+
+@pytest.mark.asyncio
+async def test_get_admin1_result(event_loop, refresh_db):
+    log.info('started test_get_admin1_result')
+
+    async with AsyncClient(app=app, base_url='http://test', params=query_parameters) as ac:
+        response = await ac.get(ENDPOINT_ROUTER)
+
+    for field in expected_fields:
+        assert field in response.json()[0], f'Field "{field}" not found in the response'
+
+    assert len(response.json()[0]) == len(expected_fields), f'Response has a different number of fields than expected'
