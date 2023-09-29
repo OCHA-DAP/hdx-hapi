150082258d4859567318964a1010bdca86487f4c
Catalin <catalinflorentinion@gmail.com>
2023-09-29 08:59:55 +0000

    HAPI-129 review age range params + add more tests

--- hdx_hapi/endpoints/get_demographic.py
+++ hdx_hapi/endpoints/get_demographic.py
@@ -20,8 +20,6 @@ async def get_age_ranges(
     pagination_parameters: Annotated[dict, Depends(pagination_parameters)],
     db: AsyncSession = Depends(get_db),
     code: Annotated[str, Query(max_length=32, description='Age range code', example='20-24')] = None,
-    age_min: Annotated[int, Query(ge=0, description='Minimum age', example=10)] = None,
-    age_max: Annotated[int, Query(ge=0, description='Maximum age', example=50)] = None
 ):
     """Get the list of all active age ranges.
     """    
@@ -29,8 +27,6 @@ async def get_age_ranges(
         pagination_parameters=pagination_parameters,
         db=db,
         code=code,
-        age_min=age_min,
-        age_max=age_max
     )
     return result
 
--- tests/test_endpoints/test_age_range_endpoint.py
+++ tests/test_endpoints/test_age_range_endpoint.py
@@ -14,4 +14,25 @@ async def test_get_age_ranges(event_loop, refresh_db):
         response = await ac.get('/api/age_range')
     assert response.status_code == 200
     assert len(response.json()) > 0, 'There should be at least one age range in the database'
-    
+
+
+@pytest.mark.asyncio
+async def test_get_age_range_params(event_loop, refresh_db):
+    log.info('started test_get_age_range_params')
+
+    params = {
+        'code': '10-14'
+    }
+
+    for param_name, param_value in params.items():
+        async with AsyncClient(app=app, base_url="http://test", params={param_name: param_value}) as ac:
+            response = await ac.get('/api/age_range')
+
+        assert response.status_code == 200
+        assert len(response.json()) > 0, f'There should be at least one age_range entry for parameter "{param_name}" with value "{param_value}" in the database'
+
+    async with AsyncClient(app=app, base_url="http://test", params=params) as ac:
+        response = await ac.get('/api/age_range')
+
+    assert response.status_code == 200
+    assert len(response.json()) > 0, f'There should be at least one age_range entry for all parameters in the database'
