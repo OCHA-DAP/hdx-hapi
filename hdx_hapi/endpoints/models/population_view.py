98106344312da8811338d01230d6e60931a58d1e
Dan Mihaila <danmihaila@gmail.com>
2023-09-29 16:34:18 +0300

    population nullable fields

--- hdx_hapi/endpoints/models/population_view.py
+++ hdx_hapi/endpoints/models/population_view.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class PopulationViewPydantic(BaseModel):
-    gender_code: str = Field(max_length=1)
-    age_range_code: str = Field(max_length=32)
+    gender_code: Optional[str] = Field(max_length=1,required=False)
+    age_range_code: Optional[str] = Field(max_length=32,required=False)
     population: int
     dataset_hdx_stub: str = Field(max_length=128)
     location_code: str = Field(max_length=128)
