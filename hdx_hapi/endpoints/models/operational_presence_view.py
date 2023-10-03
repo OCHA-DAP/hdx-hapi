7900a2f3beddf07cb4ec9c8f5359274b1ae92537
Catalin <catalinflorentinion@gmail.com>
2023-10-03 17:51:37 +0000

    HAPI-188 set admin1/2 to null if unspecified for population & 3w endpoints

--- hdx_hapi/endpoints/models/operational_presence_view.py
+++ hdx_hapi/endpoints/models/operational_presence_view.py
@@ -1,5 +1,5 @@
-from pydantic import ConfigDict, Field
-from datetime import datetime
+from pydantic import ConfigDict, Field, validator
+from typing import Optional
 
 from hdx_hapi.endpoints.models.base import HapiBaseModel
 
@@ -13,10 +13,14 @@ class OperationalPresenceViewPydantic(HapiBaseModel):
     sector_name: str = Field(max_length=512)
     location_code: str = Field(max_length=128)
     location_name: str = Field(max_length=512)
-    admin1_code: str = Field(max_length=128)
-    admin1_name: str = Field(max_length=512)
-    admin2_code: str = Field(max_length=128)
-    admin2_name: str = Field(max_length=512)
+
+    admin1_is_unspecified: bool = Field(exclude=True)
+    admin2_is_unspecified: bool = Field(exclude=True)
+
+    admin1_code: Optional[str] = Field(max_length=128)
+    admin1_name: Optional[str] = Field(max_length=512)
+    admin2_code: Optional[str] = Field(max_length=128)
+    admin2_name: Optional[str] = Field(max_length=512)
     # resource_update_date: datetime
     # org_ref: int = None,
     # dataset_hdx_id: str = Field(max_length=36),
@@ -29,3 +33,25 @@ class OperationalPresenceViewPydantic(HapiBaseModel):
 
 
     model_config = ConfigDict(from_attributes=True)
+
+    @validator('admin1_code', 'admin1_name', pre=True)
+    def set_admin1_null(cls, v, values):
+        admin1_is_unspecified = values.get('admin1_is_unspecified', False)
+
+        # If 'admin1_is_unspecified' is True, set 'admin1_code' and 'admin1_name' to None
+        if admin1_is_unspecified:
+            return None
+
+        # If 'admin1_is_unspecified' is False, leave 'admin1_code' and 'admin1_name' unchanged
+        return v
+
+    @validator('admin2_code', 'admin2_name', pre=True)
+    def set_admin2_null(cls, v, values):
+        admin2_is_unspecified = values.get('admin2_is_unspecified', False)
+
+        # If 'admin2_is_unspecified' is True, set 'admin2_code' and 'admin2_name' to None
+        if admin2_is_unspecified:
+            return None
+
+        # If 'admin2_is_unspecified' is False, leave 'admin2_code' and 'admin2_name' unchanged
+        return v
--- hdx_hapi/endpoints/models/population_view.py
+++ hdx_hapi/endpoints/models/population_view.py
@@ -1,4 +1,4 @@
-from pydantic import ConfigDict, Field
+from pydantic import ConfigDict, Field, validator
 from typing import Optional
 
 from hdx_hapi.endpoints.models.base import HapiBaseModel
@@ -12,9 +12,35 @@ class PopulationViewPydantic(HapiBaseModel):
     resource_hdx_id: str = Field(max_length=36)
     location_code: str = Field(max_length=128)
     location_name: str = Field(max_length=512)
-    admin1_code: str = Field(max_length=128)
-    admin1_name: str = Field(max_length=512)
-    admin2_code: str = Field(max_length=128)
-    admin2_name: str = Field(max_length=512)
+
+    admin1_is_unspecified: bool = Field(exclude=True)
+    admin2_is_unspecified: bool = Field(exclude=True)
+
+    admin1_code: Optional[str] = Field(max_length=128)
+    admin1_name: Optional[str] = Field(max_length=512)
+    admin2_code: Optional[str] = Field(max_length=128)
+    admin2_name: Optional[str] = Field(max_length=512)
 
     model_config = ConfigDict(from_attributes=True)
+
+    @validator('admin1_code', 'admin1_name', pre=True)
+    def set_admin1_null(cls, v, values):
+        admin1_is_unspecified = values.get('admin1_is_unspecified', False)
+
+        # If 'admin1_is_unspecified' is True, set 'admin1_code' and 'admin1_name' to None
+        if admin1_is_unspecified:
+            return None
+
+        # If 'admin1_is_unspecified' is False, leave 'admin1_code' and 'admin1_name' unchanged
+        return v
+
+    @validator('admin2_code', 'admin2_name', pre=True)
+    def set_admin2_null(cls, v, values):
+        admin2_is_unspecified = values.get('admin2_is_unspecified', False)
+
+        # If 'admin2_is_unspecified' is True, set 'admin2_code' and 'admin2_name' to None
+        if admin2_is_unspecified:
+            return None
+
+        # If 'admin2_is_unspecified' is False, leave 'admin2_code' and 'admin2_name' unchanged
+        return v
