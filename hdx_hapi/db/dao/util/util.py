6c00cb7057f1152ab6f0bd143f967c9a5373725e
Catalin <catalinflorentinion@gmail.com>
2023-10-11 13:04:32 +0000

    HAPI-208 add param type

--- hdx_hapi/db/dao/util/util.py
+++ hdx_hapi/db/dao/util/util.py
@@ -1,5 +1,5 @@
 from typing import Dict
-from sqlalchemy import Select
+from sqlalchemy import Column, Select
 
 def apply_pagination(query: Select, pagination_parameters: Dict) -> Select:
     offset = pagination_parameters.get('offset')
@@ -11,6 +11,6 @@ def apply_pagination(query: Select, pagination_parameters: Dict) -> Select:
 
     return query.limit(limit).offset(offset)
 
-def case_insensitive_filter(query: Select, column, value) -> Select:
+def case_insensitive_filter(query: Select, column: Column, value: str) -> Select:
     query = query.where(column.ilike(value))
     return query
