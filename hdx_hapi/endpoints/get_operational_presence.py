0da693bc441eacc944935774db1e37ef6db33663
CJ Hendrix <hendrix@un.org>
2023-10-16 18:00:19 +0200

    HAPI-179 mkdocs and openapi docs (#46)
    * minor formatting change to mkdocs
    
    * adjustments to 3w and admin-level
    
    * HAPI-179 more docs enrichment
    
    ---------
    
    Co-authored-by: Alexandru Gartner <alexandru-m-g@users.noreply.github.com>

--- hdx_hapi/endpoints/get_operational_presence.py
+++ hdx_hapi/endpoints/get_operational_presence.py
@@ -12,15 +12,15 @@ from hdx_hapi.services.operational_presence_logic import get_operational_presenc
 from hdx_hapi.services.sql_alchemy_session import get_db
 
 router = APIRouter(
-    tags=['3W'],
+    tags=['3W Operational Presence'],
 )
 
 @router.get('/api/themes/3w', response_model=List[OperationalPresenceViewPydantic])
-@router.get('/api/themes/3W', response_model=List[OperationalPresenceViewPydantic])
+@router.get('/api/themes/3W', response_model=List[OperationalPresenceViewPydantic], summary="Additional version of this endpoint to support the uppercase 'W'")
 async def get_operational_presences(
     pagination_parameters: Annotated[dict, Depends(pagination_parameters)],
     db: AsyncSession = Depends(get_db),
-    sector_code: Annotated[str, Query(max_length=512, description='Sector Code')] = None,
+    sector_code: Annotated[str, Query(max_length=512, description='Sector codes describe the humanitarian sector in which the operational presence is working. See the <a href="https://FIXTHIS/docs#/humanitarian-response/get_sectors_api_sector_get">sector endpoint</a> for details')] = None,
     dataset_provider_code: Annotated[str, Query(max_length=128, description='Organization(provider) code')] = None,
     resource_update_date_min: Annotated[datetime | date, Query(description='Min date of update date, e.g. 2020-01-01 or 2020-01-01T00:00:00', example='2020-01-01')] = None,
     resource_update_date_max: Annotated[datetime | date, Query(description='Max date of update date, e.g. 2024-12-31 or 2024-12-31T23:59:59', example='2024-12-31')] = None,
@@ -48,7 +48,9 @@ async def get_operational_presences(
     output_format: OutputFormat = OutputFormat.JSON,
 ):
     """
-    Get the list of operational presences.
+    UNOCHA's 3W (Who is doing What Where) Operational Presence data provide 
+    information about which organizations are working in different locations affected by a 
+    crisis. <a href="https://3w.unocha.org/">Learn more about 3W</a>
     """
     result = await get_operational_presences_srv(
         pagination_parameters=pagination_parameters, 
