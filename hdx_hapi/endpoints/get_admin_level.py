44d5e442089d2ccf9e24dc80de40c98f19a90210
CJ Hendrix <hendrix@un.org>
2023-11-01 13:40:56 +0000

    HDX-179 Text tweaks to admin, main, and 3w

--- hdx_hapi/endpoints/get_admin_level.py
+++ hdx_hapi/endpoints/get_admin_level.py
@@ -15,19 +15,19 @@ from hdx_hapi.services.sql_alchemy_session import get_db
 from datetime import datetime, date
 
 router = APIRouter(
-    tags=['Location and Administrative Divisions'],
+    tags=['Locations and Administrative Divisions'],
 )
 
-@router.get('/api/location', response_model=List[LocationResponse])
+@router.get('/api/location', response_model=List[LocationResponse], summary='Get the list of locations (typically countries) included in HAPI.')
 async def get_locations(
     pagination_parameters: Annotated[dict, Depends(pagination_parameters)],
     db: AsyncSession = Depends(get_db),
-    code: Annotated[str, Query(max_length=128, description='Location code')] = None,
-    name: Annotated[str, Query(max_length=512, description='Location name')] = None,
+    code: Annotated[str, Query(max_length=128, description='Filter the response by a location (typically a country). The location codes use the ISO-3 (ISO 3166 alpha-3) codes. See the <a href="https://FIXTHIS/docs#/Location%20and%20Administrative%20Divisions/get_locations_api_location_get">location endpoint</a> for details')] = None,
+    name: Annotated[str, Query(max_length=512, description='Filter the response by a location (typically a country). The location names are based on the "short name" from the <a href="https://unstats.un.org/unsd/methodology/m49/#fn2">UN M49 Standard</a> . See the <a href="https://FIXTHIS/docs#/Location%20and%20Administrative%20Divisions/get_locations_api_location_get">location endpoint</a> for details.')] = None,
 
     output_format: OutputFormat = OutputFormat.JSON,
 ):
-    """Get the list of locations.
+    """Not all data are available for all locations. Learn more about the scope of data coverage in HAPI in the <a href="https://FIXTHIS/">Overview and Getting Started documentation.</a> 
     """    
     result = await get_locations_srv(
         pagination_parameters=pagination_parameters,
@@ -42,8 +42,8 @@ async def get_locations(
 async def get_admin1(
     pagination_parameters: Annotated[dict, Depends(pagination_parameters)],
     db: AsyncSession = Depends(get_db),
-    code: Annotated[str, Query(max_length=128, description='Code')] = None,
-    name: Annotated[str, Query(max_length=512, description='Name')] = None,
+    code: Annotated[str, Query(max_length=128, description='Filter the response by the 1st subnational administrative divisions. The admin1 codes refer to the p-codes in the Common Operational Datasets.')] = None,
+    name: Annotated[str, Query(max_length=512, description='Filter the response by the 1st subnational administrative divisions. The admin2 names refer to the Common Operational Datasets. See the <a href="https://FIXTHIS/docs#/Location%20and%20Administrative%20Divisions/get_admin1_api_admin1_get">admin1 endpoint</a> for details.')] = None,
     location_code: Annotated[str, Query(max_length=128, description='Location code')] = None,
     location_name: Annotated[str, Query(max_length=512, description='Location name')] = None,
 
