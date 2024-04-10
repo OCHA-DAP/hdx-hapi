e339b48b1bb9f1c52905e7229d7024da05d92543
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-04-10 13:09:10 +0100

    Fix line too long errors introduced by Ruff auto-formatting(?) removing #noqa tags

--- hdx_hapi/endpoints/get_operational_presence.py
+++ hdx_hapi/endpoints/get_operational_presence.py
@@ -26,27 +26,32 @@ router = APIRouter(
     tags=['3W Operational Presence'],
 )
 
+SUMMARY_TEXT = (
+    'Get the list of organizations present and in which humanitarian sectors they are working. '
+    "There are two versions of this endpoint to support the uppercase and lowercase 'w'"
+)
+
 
 @router.get(
     '/api/themes/3w',
     response_model=List[OperationalPresenceResponse],
-    summary="Get the list of organizations present and in which humanitarian sectors they are working. There are two versions of this endpoint to support the uppercase and lowercase 'w'",
-)  # noqa
+    summary=SUMMARY_TEXT,
+)
 @router.get(
     '/api/themes/3W',
     response_model=List[OperationalPresenceResponse],
-    summary="Get the list of organizations present and in which humanitarian sectors they are working. There are two versions of this endpoint to support the uppercase and lowercase 'w'.",
-)  # noqa
+    summary=SUMMARY_TEXT,
+)
 @router.get(
     '/api/v1/themes/3w',
     response_model=List[OperationalPresenceResponse],
-    summary="Get the list of organizations present and in which humanitarian sectors they are working. There are two versions of this endpoint to support the uppercase and lowercase 'w'",
-)  # noqa
+    summary=SUMMARY_TEXT,
+)
 @router.get(
     '/api/v1/themes/3W',
     response_model=List[OperationalPresenceResponse],
-    summary="Get the list of organizations present and in which humanitarian sectors they are working. There are two versions of this endpoint to support the uppercase and lowercase 'w'.",
-)  # noqa
+    summary=SUMMARY_TEXT,
+)
 async def get_operational_presences(
     pagination_parameters: Annotated[dict, Depends(pagination_parameters)],
     db: AsyncSession = Depends(get_db),
@@ -54,30 +59,50 @@ async def get_operational_presences(
         str,
         Query(
             max_length=512,
-            description='Filter the response by sector codes, which describe the humanitarian sector to which the operational presence applies. See the <a href="https://FIXTHIS/docs#/humanitarian-response/get_sectors_api_sector_get">sector endpoint</a> for details',
+            description=(
+                'Filter the response by sector codes, which describe the humanitarian sector '
+                ' to which the operational presence applies. '
+                'See the <a href="https://FIXTHIS/docs#/humanitarian-response/get_sectors_api_sector_get"> '
+                'sector endpoint</a> for details'
+            ),
         ),
-    ] = None,  # noqa
+    ] = None,
     sector_name: Annotated[
         str,
         Query(
             max_length=512,
-            description='Filter the response by sector names, which describe the humanitarian sector to which the operational presence applies. See the <a href="https://FIXTHIS/docs#/humanitarian-response/get_sectors_api_sector_get">sector endpoint</a> for details.',
+            description=(
+                'Filter the response by sector names, '
+                'which describe the humanitarian sector to which the operational presence applies. '
+                'See the <a href="https://FIXTHIS/docs#/humanitarian-response/get_sectors_api_sector_get">'
+                'sector endpoint</a> for details.'
+            ),
         ),
-    ] = None,  # noqa
+    ] = None,
     org_acronym: Annotated[
         str,
         Query(
             max_length=32,
-            description='Filter the response by the acronym of the organization to which the operational presence applies. See the <a href="https://FIXTHIS/docs#/humanitarian-response/get_orgs_api_org_get">org endpoint</a> for details',
+            description=(
+                'Filter the response by the acronym of the organization '
+                'to which the operational presence applies. '
+                'See the <a href="https://FIXTHIS/docs#/humanitarian-response/get_orgs_api_org_get">'
+                'org endpoint</a> for details'
+            ),
         ),
-    ] = None,  # noqa
+    ] = None,
     org_name: Annotated[
         str,
         Query(
             max_length=512,
-            description='Filter the response by the name of the organization to which the operational presence applies. See the <a href="https://FIXTHIS/docs#/humanitarian-response/get_orgs_api_org_get">org endpoint</a> for details',
+            description=(
+                'Filter the response by the name of the organization '
+                'to which the operational presence applies. '
+                'See the <a href="https://FIXTHIS/docs#/humanitarian-response/get_orgs_api_org_get">'
+                'org endpoint</a> for details'
+            ),
         ),
-    ] = None,  # noqa
+    ] = None,
     location_code: Annotated[str, Query(max_length=128, description=f'{DOC_LOCATION_CODE} {DOC_SEE_LOC}')] = None,
     location_name: Annotated[str, Query(max_length=512, description=f'{DOC_LOCATION_NAME} {DOC_SEE_LOC}')] = None,
     admin1_code: Annotated[str, Query(max_length=128, description=f'{DOC_ADMIN1_CODE} {DOC_SEE_ADMIN1}')] = None,
@@ -90,24 +115,34 @@ async def get_operational_presences(
     resource_update_date_min: Annotated[
         NaiveDatetime | date,
         Query(
-            description='Filter the repsonse to data updated on or after this date. For example 2020-01-01 or 2020-01-01T00:00:00',
+            description=(
+                'Filter the repsonse to data updated on or after this date. '
+                'For example 2020-01-01 or 2020-01-01T00:00:00'
+            ),
             examples=['2020-01-01'],
         ),
-    ] = None,  # noqa
+    ] = None,
     resource_update_date_max: Annotated[
         NaiveDatetime | date,
         Query(
-            description='Filter the repsonse to data updated on or before this date. For example 2024-12-31 or 2024-12-31T23:59:59',
+            description=(
+                'Filter the repsonse to data updated on or before this date. '
+                'For example 2024-12-31 or 2024-12-31T23:59:59'
+            ),
             examples=['2024-12-31'],
         ),
-    ] = None,  # noqa
+    ] = None,
     dataset_hdx_provider_stub: Annotated[
         str,
         Query(
             max_length=128,
-            description='Filter the query by the organizations contributing the source data to HDX. If you want to filter by the organization mentioned in the operational presence record, see the org_name and org_acronym parameters below.',
+            description=(
+                'Filter the query by the organizations contributing the source data to HDX. '
+                'If you want to filter by the organization mentioned in the operational presence record, '
+                'see the org_name and org_acronym parameters below.'
+            ),
         ),
-    ] = None,  # noqa
+    ] = None,
     # org_ref: Annotated[int, Query(ge=1, description='Organization reference')] = None,
     # dataset_hdx_id: Annotated[str, Query(max_length=36, description='HDX Dataset ID')] = None,
     # dataset_hdx_stub: Annotated[str, Query(max_length=128, description='HDX Dataset Name')] = None,
