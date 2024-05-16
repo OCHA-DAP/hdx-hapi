cfdaa658c1cab8c3f50e2ee67e3c18eda8dc2048
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2024-05-16 21:50:48 +0000

    Adding back original sample data and check on case insensitive search + small merge fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,7 @@ from hdx_hapi.db.models.views.util.util import CreateView
 
 SAMPLE_DATA_SQL_FILES = [
     'tests/sample_location_admin.sql',
-    'tests/sample_dataset_resource.sql'
+    'tests/sample_dataset_resource.sql',
     'tests/sample_sector.sql',
     'tests/sample_org_type.sql',
     'tests/sample_org.sql',
