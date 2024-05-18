200b8a13fa467a3775d577229e897f43e86647b9
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2024-05-18 11:14:16 +0000

    HDX-9818 refactoring pydantic models with admins

--- hdx_hapi/endpoints/models/operational_presence.py
+++ hdx_hapi/endpoints/models/operational_presence.py
@@ -1,19 +1,16 @@
-from pydantic import ConfigDict, Field, model_validator, NaiveDatetime
+from pydantic import ConfigDict, Field, NaiveDatetime
 from typing import Optional
 
-from hdx_hapi.endpoints.models.base import HapiBaseModel
+from hdx_hapi.endpoints.models.base import HapiBaseModel, HapiModelWithAdmins
 
 
-class OperationalPresenceResponse(HapiBaseModel):
+class OperationalPresenceResponse(HapiBaseModel, HapiModelWithAdmins):
     # dataset_hdx_stub: str = Field(max_length=128)
     resource_hdx_id: str = Field(max_length=36)
     org_acronym: str = Field(max_length=32)
     org_name: str = Field(max_length=512)
     sector_code: str = Field(max_length=32)
     sector_name: str = Field(max_length=512)
-    location_ref: int
-    location_code: str = Field(max_length=128)
-    location_name: str = Field(max_length=512)
 
     reference_period_start: NaiveDatetime
     reference_period_end: Optional[NaiveDatetime]
@@ -21,15 +18,6 @@ class OperationalPresenceResponse(HapiBaseModel):
     # hapi_updated_date: datetime
     # hapi_replaced_date: Optional[datetime]
 
-    admin1_is_unspecified: bool = Field(exclude=True)
-    admin2_is_unspecified: bool = Field(exclude=True)
-
-    admin1_ref: int
-    admin1_code: Optional[str] = Field(max_length=128)
-    admin1_name: Optional[str] = Field(max_length=512)
-    admin2_ref: int
-    admin2_code: Optional[str] = Field(max_length=128)
-    admin2_name: Optional[str] = Field(max_length=512)
     # resource_update_date: datetime
     # org_ref: int = None,
     # dataset_hdx_id: str = Field(max_length=36),
@@ -41,20 +29,3 @@ class OperationalPresenceResponse(HapiBaseModel):
     # org_type_description: str = Field(max_length=512),
 
     model_config = ConfigDict(from_attributes=True)
-
-    @model_validator(mode='after')  # type: ignore
-    def set_admin1_admin2_null(self) -> 'OperationalPresenceResponse':
-        admin1_is_unspecified = self.admin1_is_unspecified
-        admin2_is_unspecified = self.admin2_is_unspecified
-
-        # If 'admin1_is_unspecified' is True, set 'admin1_code' and 'admin1_name' to None
-        if admin1_is_unspecified:
-            self.admin1_code = None
-            self.admin1_name = None
-
-        # If 'admin2_is_unspecified' is True, set 'admin2_code' and 'admin2_name' to None
-        if admin2_is_unspecified:
-            self.admin2_code = None
-            self.admin2_name = None
-
-        return self
