09ff43744916d027a2aaaef8bdc05748feb3f363
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-05-19 11:06:36 +0100

    HDX-9829 Working my way from route to service to populations_view_list call

--- hdx_hapi/endpoints/get_population.py
+++ hdx_hapi/endpoints/get_population.py
@@ -10,17 +10,18 @@ from hdx_hapi.config.doc_snippets import (
     DOC_LOCATION_CODE,
     DOC_LOCATION_NAME,
     DOC_SEE_LOC,
-    DOC_UPDATE_DATE_MAX,
-    DOC_UPDATE_DATE_MIN,
-    DOC_HAPI_UPDATED_DATE_MIN,
-    DOC_HAPI_UPDATED_DATE_MAX,
-    DOC_HAPI_REPLACED_DATE_MIN,
-    DOC_HAPI_REPLACED_DATE_MAX,
 )
 
 from hdx_hapi.endpoints.models.base import HapiGenericResponse
 from hdx_hapi.endpoints.models.population import PopulationResponse
-from hdx_hapi.endpoints.util.util import AdminLevel, CommonEndpointParams, OutputFormat, common_endpoint_parameters
+from hdx_hapi.endpoints.util.util import (
+    CommonEndpointParams,
+    OutputFormat,
+    ReferencePeriodParameters,
+    common_endpoint_parameters,
+    reference_period_parameters,
+    AdminLevel,
+)
 from hdx_hapi.services.csv_transform_logic import transform_result_to_csv_stream_if_requested
 from hdx_hapi.services.population_logic import get_populations_srv
 from hdx_hapi.services.sql_alchemy_session import get_db
@@ -43,44 +44,23 @@ router = APIRouter(
 )
 async def get_populations(
     common_parameters: Annotated[CommonEndpointParams, Depends(common_endpoint_parameters)],
+    ref_period_parameters: Annotated[ReferencePeriodParameters, Depends(reference_period_parameters)],
     db: AsyncSession = Depends(get_db),
-    gender_code: Annotated[str, Query(max_length=1, description='Gender code')] = None,
-    age_range_code: Annotated[str, Query(max_length=32, description='Age range code')] = None,
+    gender: Annotated[str, Query(max_length=1, description='Gender')] = None,
+    age_range: Annotated[str, Query(max_length=32, description='Age range')] = None,
+    min_age: Annotated[int, Query(description='Minimum age')] = None,
+    max_age: Annotated[int, Query(description='Maximum age')] = None,
     population: Annotated[int, Query(description='Population')] = None,
-    dataset_hdx_provider_stub: Annotated[str, Query(max_length=128, description='Organization(provider) code')] = None,
-    resource_update_date_min: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_UPDATE_DATE_MIN}', openapi_examples={'2020-01-01': {'value': '2020-01-01'}}),
-    ] = None,
-    resource_update_date_max: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_UPDATE_DATE_MAX}', openapi_examples={'2024-12-31': {'value': '2024-12-31'}}),
-    ] = None,
-    hapi_updated_date_min: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_HAPI_UPDATED_DATE_MIN}'),
-    ] = None,
-    hapi_updated_date_max: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_HAPI_UPDATED_DATE_MAX}'),
-    ] = None,
-    hapi_replaced_date_min: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_HAPI_REPLACED_DATE_MIN}'),
-    ] = None,
-    hapi_replaced_date_max: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_HAPI_REPLACED_DATE_MAX}'),
-    ] = None,
+    admin1_ref: Annotated[int, Query(description='Admin1 reference')] = None,
+    location_ref: Annotated[int, Query(description='Location reference')] = None,
     location_code: Annotated[str, Query(max_length=128, description=f'{DOC_LOCATION_CODE} {DOC_SEE_LOC}')] = None,
     location_name: Annotated[str, Query(max_length=512, description=f'{DOC_LOCATION_NAME} {DOC_SEE_LOC}')] = None,
     admin1_name: Annotated[str, Query(max_length=512, description='Admin1 name')] = None,
     admin1_code: Annotated[str, Query(max_length=128, description='Admin1 code')] = None,
     # admin1_is_unspecified: Annotated[bool, Query(description='Is admin1 specified or not')] = None,
-    location_ref: Annotated[int, Query(description='Location reference')] = None,
+    admin2_ref: Annotated[int, Query(description='Admin2 reference')] = None,
     admin2_name: Annotated[str, Query(max_length=512, description='Admin2 name')] = None,
     admin2_code: Annotated[str, Query(max_length=128, description='Admin2 code')] = None,
-    admin1_ref: Annotated[int, Query(description='Admin1 reference')] = None,
     admin_level: Annotated[AdminLevel, Query(description='Filter the response by admin level')] = None,
     # admin2_is_unspecified: Annotated[bool, Query(description='Is admin2 specified or not')] = None,
     output_format: OutputFormat = OutputFormat.JSON,
@@ -89,28 +69,23 @@ async def get_populations(
     Return the list of populations
     """
     result = await get_populations_srv(
-        pagination_parameters=common_parameters,
+        common_parameters=common_parameters,
+        ref_period_parameters=ref_period_parameters,
         db=db,
-        gender_code=gender_code,
-        age_range_code=age_range_code,
+        gender=gender,
+        age_range=age_range,
+        min_age=min_age,
+        max_age=max_age,
         population=population,
-        dataset_hdx_provider_stub=dataset_hdx_provider_stub,
-        resource_update_date_min=resource_update_date_min,
-        resource_update_date_max=resource_update_date_max,
-        hapi_updated_date_min=hapi_updated_date_min,
-        hapi_updated_date_max=hapi_updated_date_max,
-        hapi_replaced_date_min=hapi_replaced_date_min,
-        hapi_replaced_date_max=hapi_replaced_date_max,
+        admin1_ref=admin1_ref,
+        location_ref=location_ref,
         location_code=location_code,
         location_name=location_name,
         admin1_name=admin1_name,
         admin1_code=admin1_code,
-        location_ref=location_ref,
-        # admin1_is_unspecified=admin1_is_unspecified,
-        admin2_code=admin2_code,
+        admin2_ref=admin2_ref,
         admin2_name=admin2_name,
-        admin1_ref=admin1_ref,
+        admin2_code=admin2_code,
         admin_level=admin_level,
-        # admin2_is_unspecified=admin2_is_unspecified,
     )
     return transform_result_to_csv_stream_if_requested(result, output_format, PopulationResponse)
--- tests/test_route_calls/test_get_populations_route.py
+++ tests/test_route_calls/test_get_populations_route.py
@@ -1,11 +1,11 @@
 import pytest
 from hdx_hapi.endpoints.get_population import get_populations
 from hdx_hapi.endpoints.util.util import (
-    AdminLevel,
-    CommonEndpointParams,
+    PaginationParams,
     OutputFormat,
     common_endpoint_parameters,
-    PaginationParams,
+    reference_period_parameters,
+    AdminLevel,
 )
 
 
@@ -14,26 +14,27 @@ async def test_get_populations(event_loop, refresh_db, session_maker):
     db = session_maker()
     pagination_parameters = PaginationParams(limit=1000, offset=0)
     common_parameters = await common_endpoint_parameters(pagination_parameters=pagination_parameters)
-    result = get_populations(
+    results = get_populations(
         common_parameters=common_parameters,
+        ref_period_parameters=reference_period_parameters,
         db=db,
-        gender_code=None,
-        age_range_code=None,
-        dataset_hdx_provider_stub=None,
-        resource_update_date_min=None,
-        resource_update_date_max=None,
-        hapi_replaced_date_min=None,
-        hapi_replaced_date_max=None,
+        admin2_ref=None,
+        gender=None,
+        age_range=None,
+        min_age=None,
+        max_age=None,
+        population=None,
+        location_ref=None,
         location_code=None,
         location_name=None,
         admin1_ref=None,
+        admin1_code=None,
         admin1_name=None,
-        location_ref=None,
         admin2_name=None,
         admin2_code=None,
         admin_level=AdminLevel.ONE,
         output_format=OutputFormat.JSON,
     )
 
-    print(await result, flush=True)
-    assert False
+    print(results, flush=True)
+    assert True
