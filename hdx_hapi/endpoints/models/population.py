45b324129b877b2cc8e6eaa25ffbbd3dcdf05bc6
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-05-21 11:35:54 +0100

    HDX-9829 use correct inheritance for response which fixes tests.

--- hdx_hapi/endpoints/models/population.py
+++ hdx_hapi/endpoints/models/population.py
@@ -2,10 +2,10 @@ from pydantic import ConfigDict, Field, NaiveDatetime
 from typing import Optional
 
 from hapi_schema.utils.enums import Gender
-from hdx_hapi.endpoints.models.base import HapiBaseModel
+from hdx_hapi.endpoints.models.base import HapiBaseModel, HapiModelWithAdmins
 
 
-class PopulationResponse(HapiBaseModel):
+class PopulationResponse(HapiBaseModel, HapiModelWithAdmins):
     resource_hdx_id: str = Field(max_length=36)
     admin2_ref: int = None
 
@@ -18,18 +18,5 @@ class PopulationResponse(HapiBaseModel):
 
     reference_period_start: Optional[NaiveDatetime]
     reference_period_end: Optional[NaiveDatetime]
-
-    location_ref: int = None
-    location_code: str = Field(max_length=128)
-    location_name: str = Field(max_length=512)
-
-    admin1_ref: int = None
-    admin1_code: Optional[str] = Field(max_length=128)
-    admin1_name: Optional[str] = Field(max_length=512)
-    admin1_is_unspecified: bool = Field(exclude=True)
-
-    admin2_code: Optional[str] = Field(max_length=128)
-    admin2_name: Optional[str] = Field(max_length=512)
-    admin2_is_unspecified: bool = Field(exclude=True)
 
     model_config = ConfigDict(from_attributes=True)
