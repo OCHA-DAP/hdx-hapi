88c6f09a0e5df933e2938814ee4ce100cf1c765d
Dan Mihaila <danmihaila@gmail.com>
2024-05-21 14:14:40 +0300

    HDX-9831 -  improve pydantic validation

--- hdx_hapi/endpoints/models/humanitarian_needs.py
+++ hdx_hapi/endpoints/models/humanitarian_needs.py
@@ -8,23 +8,17 @@ from hapi_schema.utils.enums import Gender, PopulationGroup, PopulationStatus, D
 
 class HumanitarianNeedsResponse(HapiBaseModel, HapiModelWithAdmins):
     resource_hdx_id: str = Field(max_length=36)
-    gender: Optional[Gender] = Field(max_length=32)
+    gender: Gender
     age_range: Optional[str] = Field(max_length=32)
-    min_age: Optional[int] = None
-    max_age: Optional[int] = None
-    disabled_marker: Optional[DisabledMarker] = Field(max_length=32)
+    min_age: Optional[int] = Field(ge=0)
+    max_age: Optional[int] = Field(ge=0)
+    disabled_marker: DisabledMarker
     sector_code: Optional[str] = Field(max_length=32)
-    population_group: Optional[PopulationGroup] = Field(max_length=32)
-    population_status: Optional[PopulationStatus] = Field(max_length=32)
-    population: Optional[int] = None
+    population_group: PopulationGroup
+    population_status: PopulationStatus
+    population: int = Field(ge=0)
     reference_period_start: NaiveDatetime
     reference_period_end: Optional[NaiveDatetime]
     sector_name: Optional[str] = Field(max_length=512)
-
-    # dataset_hdx_stub: str = Field(max_length=128)
-    # dataset_hdx_provider_stub: str = Field(max_length=128)
-
-    # hapi_updated_date: datetime
-    # hapi_replaced_date: Optional[datetime]
 
     model_config = ConfigDict(from_attributes=True)
