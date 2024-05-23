05e41afcedd42482030f1463785ff6a5125d1c87
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-05-23 08:31:03 +0100

    HDX-9847 Work in progress whilst I checkout another branch

--- hdx_hapi/db/dao/food_security_view_dao.py
+++ hdx_hapi/db/dao/food_security_view_dao.py
@@ -1,24 +1,23 @@
-import datetime
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
-from hdx_hapi.db.models.views.db_food_security_view import FoodSecurityView
-from hdx_hapi.db.dao.util.util import apply_location_admin_filter, apply_pagination, case_insensitive_filter
-from hdx_hapi.endpoints.util.util import PaginationParams
+from hdx_hapi.db.models.views.all_views import FoodSecurityView
+from hdx_hapi.db.dao.util.util import (
+    ReferencePeriodParameters,
+    PaginationParams,
+    apply_location_admin_filter,
+    apply_reference_period_filter,
+    apply_pagination,
+    case_insensitive_filter,
+)
 
 
 async def food_security_view_list(
     pagination_parameters: PaginationParams,
+    ref_period_parameters: ReferencePeriodParameters,
     db: AsyncSession,
     ipc_phase_code: str = None,
     ipc_type_code: str = None,
-    dataset_hdx_provider_stub: str = None,
-    resource_update_date_min: datetime = None,
-    resource_update_date_max: datetime = None,
-    hapi_updated_date_min: datetime = None,
-    hapi_updated_date_max: datetime = None,
-    hapi_replaced_date_min: datetime = None,
-    hapi_replaced_date_max: datetime = None,
     location_code: str = None,
     location_name: str = None,
     admin1_name: str = None,
@@ -37,20 +36,6 @@ async def food_security_view_list(
         query = query.where(FoodSecurityView.ipc_phase_code == ipc_phase_code)
     if ipc_type_code:
         query = case_insensitive_filter(query, FoodSecurityView.ipc_type_code, ipc_type_code)
-    if dataset_hdx_provider_stub:
-        query = case_insensitive_filter(query, FoodSecurityView.dataset_hdx_provider_stub, dataset_hdx_provider_stub)
-    if resource_update_date_min:
-        query = query.where(FoodSecurityView.resource_update_date >= resource_update_date_min)
-    if resource_update_date_max:
-        query = query.where(FoodSecurityView.resource_update_date < resource_update_date_max)
-    if hapi_updated_date_min:
-        query = query.where(FoodSecurityView.hapi_updated_date >= hapi_updated_date_min)
-    if hapi_updated_date_max:
-        query = query.where(FoodSecurityView.hapi_updated_date < hapi_updated_date_max)
-    if hapi_replaced_date_min:
-        query = query.where(FoodSecurityView.hapi_replaced_date >= hapi_replaced_date_min)
-    if hapi_replaced_date_max:
-        query = query.where(FoodSecurityView.hapi_replaced_date < hapi_replaced_date_max)
 
     query = apply_location_admin_filter(
         query,
@@ -68,6 +53,7 @@ async def food_security_view_list(
         admin2_is_unspecified,
     )
 
+    query = apply_reference_period_filter(query, ref_period_parameters, FoodSecurityView)
     query = apply_pagination(query, pagination_parameters)
 
     result = await db.execute(query)
--- hdx_hapi/endpoints/get_food_security.py
+++ hdx_hapi/endpoints/get_food_security.py
@@ -5,7 +5,7 @@ from pydantic import NaiveDatetime
 
 
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from hapi_schema.utils.enums import Gender, IPCPhase, IPCType
 from hdx_hapi.config.doc_snippets import (
     DOC_LOCATION_CODE,
     DOC_LOCATION_NAME,
@@ -18,9 +18,17 @@ from hdx_hapi.config.doc_snippets import (
     DOC_HAPI_REPLACED_DATE_MAX,
 )
 
+from hdx_hapi.endpoints.models import population
 from hdx_hapi.endpoints.models.base import HapiGenericResponse
 from hdx_hapi.endpoints.models.food_security import FoodSecurityResponse
-from hdx_hapi.endpoints.util.util import AdminLevel, CommonEndpointParams, OutputFormat, common_endpoint_parameters
+from hdx_hapi.endpoints.util.util import (
+    CommonEndpointParams,
+    OutputFormat,
+    ReferencePeriodParameters,
+    common_endpoint_parameters,
+    reference_period_parameters,
+    AdminLevel,
+)
 from hdx_hapi.services.csv_transform_logic import transform_result_to_csv_stream_if_requested
 from hdx_hapi.services.food_security_logic import get_food_security_srv
 from hdx_hapi.services.sql_alchemy_session import get_db
@@ -31,55 +39,34 @@ router = APIRouter(
 
 
 @router.get(
-    '/api/themes/food_security',
+    '/api/food/food_security',
     response_model=HapiGenericResponse[FoodSecurityResponse],
     summary='Get food security data',
     include_in_schema=False,
 )
 @router.get(
-    '/api/v1/themes/food_security',
+    '/api/v1/food/food_security',
     response_model=HapiGenericResponse[FoodSecurityResponse],
     summary='Get food security data',
 )
 async def get_food_security(
     common_parameters: Annotated[CommonEndpointParams, Depends(common_endpoint_parameters)],
+    ref_period_parameters: Annotated[ReferencePeriodParameters, Depends(reference_period_parameters)],
     db: AsyncSession = Depends(get_db),
-    ipc_phase_code: Annotated[str, Query(max_length=32, description='IPC phase code')] = None,
-    ipc_type_code: Annotated[str, Query(max_length=32, description='IPC type code')] = None,
-    dataset_hdx_provider_stub: Annotated[str, Query(max_length=128, description='Organization(provider) code')] = None,
-    resource_update_date_min: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_UPDATE_DATE_MIN}', openapi_examples={'2020-01-01': {'value': '2020-01-01'}}),
-    ] = None,
-    resource_update_date_max: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_UPDATE_DATE_MAX}', openapi_examples={'2024-12-31': {'value': '2024-12-31'}}),
-    ] = None,
-    hapi_updated_date_min: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_HAPI_UPDATED_DATE_MIN}'),
-    ] = None,
-    hapi_updated_date_max: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_HAPI_UPDATED_DATE_MAX}'),
-    ] = None,
-    hapi_replaced_date_min: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_HAPI_REPLACED_DATE_MIN}'),
-    ] = None,
-    hapi_replaced_date_max: Annotated[
-        NaiveDatetime | date,
-        Query(description=f'{DOC_HAPI_REPLACED_DATE_MAX}'),
-    ] = None,
+    ipc_phase: Annotated[IPCPhase, Query(description='IPC Phase')] = None,
+    ipc_type: Annotated[IPCType, Query(description='IPC Type')] = None,
+    admin1_ref: Annotated[int, Query(description='Admin1 reference')] = None,
+    location_ref: Annotated[int, Query(description='Location reference')] = None,
     location_code: Annotated[str, Query(max_length=128, description=f'{DOC_LOCATION_CODE} {DOC_SEE_LOC}')] = None,
     location_name: Annotated[str, Query(max_length=512, description=f'{DOC_LOCATION_NAME} {DOC_SEE_LOC}')] = None,
     admin1_name: Annotated[str, Query(max_length=512, description='Admin1 name')] = None,
     admin1_code: Annotated[str, Query(max_length=128, description='Admin1 code')] = None,
-    location_ref: Annotated[int, Query(description='Location reference')] = None,
+    # admin1_is_unspecified: Annotated[bool, Query(description='Is admin1 specified or not')] = None,
+    admin2_ref: Annotated[int, Query(description='Admin2 reference')] = None,
     admin2_name: Annotated[str, Query(max_length=512, description='Admin2 name')] = None,
     admin2_code: Annotated[str, Query(max_length=128, description='Admin2 code')] = None,
-    admin1_ref: Annotated[int, Query(description='Admin1 reference')] = None,
     admin_level: Annotated[AdminLevel, Query(description='Filter the response by admin level')] = None,
+    # admin2_is_unspecified: Annotated[bool, Query(description='Is admin2 specified or not')] = None,
     output_format: OutputFormat = OutputFormat.JSON,
 ):
     """
@@ -87,21 +74,16 @@ async def get_food_security(
     """
     result = await get_food_security_srv(
         pagination_parameters=common_parameters,
+        ref_period_parameters=ref_period_parameters,
         db=db,
-        ipc_phase_code=ipc_phase_code,
-        ipc_type_code=ipc_type_code,
-        dataset_hdx_provider_stub=dataset_hdx_provider_stub,
-        resource_update_date_min=resource_update_date_min,
-        resource_update_date_max=resource_update_date_max,
-        hapi_updated_date_min=hapi_updated_date_min,
-        hapi_updated_date_max=hapi_updated_date_max,
-        hapi_replaced_date_min=hapi_replaced_date_min,
-        hapi_replaced_date_max=hapi_replaced_date_max,
+        ipc_phase=ipc_phase,
+        ipc_type=ipc_type,
         location_code=location_code,
         location_name=location_name,
         admin1_name=admin1_name,
         admin1_code=admin1_code,
         location_ref=location_ref,
+        admin2_ref=admin2_ref,
         admin2_code=admin2_code,
         admin2_name=admin2_name,
         admin1_ref=admin1_ref,
