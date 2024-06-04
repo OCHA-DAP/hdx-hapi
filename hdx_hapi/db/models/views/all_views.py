131c619e68ac4071671ffb06c962e5f0234fd8e1
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-06-04 11:50:19 +0100

    HDX-9881 Trying to change the response model

--- hdx_hapi/db/models/views/all_views.py
+++ hdx_hapi/db/models/views/all_views.py
@@ -131,7 +131,7 @@ class CurrencyView(Base):
 
 class DatasetView(Base):
     __table__ = dataset_view
-    hdx_id: Mapped[str] = column_property(dataset_view.c.hdx_id)
+    hdx_id: Mapped[str] = column_property(dataset_view.c.hdx_id.label('dataset_hdx_id'))
     hdx_stub: Mapped[str] = column_property(dataset_view.c.hdx_stub)
     title: Mapped[str] = column_property(dataset_view.c.title)
     hdx_provider_stub: Mapped[str] = column_property(dataset_view.c.hdx_provider_stub)
