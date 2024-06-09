4247ccb30234ae8537e8a394967e8fa6a3833b16
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-06-09 13:34:04 +0100

    HDX-9884 Finish documenting the response classes

--- hdx_hapi/endpoints/models/refugees.py
+++ hdx_hapi/endpoints/models/refugees.py
@@ -1,15 +1,27 @@
 from pydantic import ConfigDict, Field, NaiveDatetime
 from typing import Optional
 
-from hdx_hapi.config.doc_snippets import DOC_GENDER, DOC_POPULATION_GROUP, DOC_AGE_RANGE, truncate_query_description
+from hdx_hapi.config.doc_snippets import (
+    DOC_GENDER,
+    DOC_HDX_RESOURCE_ID,
+    DOC_POPULATION_GROUP,
+    DOC_AGE_RANGE,
+    DOC_REFERENCE_PERIOD_END,
+    DOC_REFERENCE_PERIOD_START,
+    truncate_query_description,
+)
 from hdx_hapi.endpoints.models.base import HapiBaseModel
 from hapi_schema.utils.enums import Gender, PopulationGroup
 
 
 class RefugeesResponse(HapiBaseModel):
-    resource_hdx_id: str = Field(max_length=36)
-    origin_location_ref: int
-    asylum_location_ref: int
+    resource_hdx_id: str = Field(max_length=36, description=truncate_query_description(DOC_HDX_RESOURCE_ID))
+    origin_location_ref: int = Field(
+        description='An internal, stable identifier that references the location of origin'
+    )
+    asylum_location_ref: int = Field(
+        description='An internal, stable identifier that references the location of asylum'
+    )
     population_group: PopulationGroup = Field(description=truncate_query_description(DOC_POPULATION_GROUP))
     gender: Gender = Field(description=truncate_query_description(DOC_GENDER))
     age_range: str = Field(max_length=32, description=truncate_query_description(DOC_AGE_RANGE))
@@ -28,12 +40,12 @@ class RefugeesResponse(HapiBaseModel):
             'age range'
         ),
     )
-    population: int = Field(ge=0)
-    reference_period_start: NaiveDatetime
-    reference_period_end: Optional[NaiveDatetime]
-    origin_location_code: str = Field(max_length=128)
-    origin_location_name: str = Field(max_length=512)
-    asylum_location_code: str = Field(max_length=128)
-    asylum_location_name: str = Field(max_length=512)
+    population: int = Field(ge=0, description='The number of people')
+    reference_period_start: NaiveDatetime = Field(description=DOC_REFERENCE_PERIOD_START)
+    reference_period_end: Optional[NaiveDatetime] = Field(description=DOC_REFERENCE_PERIOD_END)
+    origin_location_code: str = Field(max_length=128, description='Location of origin p-code (ISO-3)')
+    origin_location_name: str = Field(max_length=512, description='Location of origin name')
+    asylum_location_code: str = Field(max_length=128, description='Location of asylum p-code (ISO-3)')
+    asylum_location_name: str = Field(max_length=512, description='Location of asylum name')
 
     model_config = ConfigDict(from_attributes=True)
