884a7c51d9186fcb28fb98300819b31222598adc
Dan Mihaila <danmihaila@gmail.com>
2024-06-12 11:44:27 +0300

    Update version.py

--- hdx_hapi/endpoints/util/version.py
+++ hdx_hapi/endpoints/util/version.py
@@ -1,7 +1,2 @@
-<<<<<<< feature/hdx-9905
-api_version = '0.4.3'
-hapi_sqlalchemy_schema_version = '0.8.12'
-=======
 api_version = '0.4.4'
-hapi_sqlalchemy_schema_version = '0.8.11'
->>>>>>> dev
+hapi_sqlalchemy_schema_version = '0.8.12'
