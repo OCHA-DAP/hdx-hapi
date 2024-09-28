a98c7b76ce7186881838aa3abd54a3961ab7187e
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-09-28 13:34:12 +0100

    HDX-10158 Update schema version and test fixtures - all tests pass

--- tests/sample_data/food_security.sql
+++ tests/sample_data/food_security.sql
@@ -1,12 +1,12 @@
-INSERT INTO public.food_security (resource_hdx_id, admin2_ref, ipc_phase, ipc_type, population_in_phase, population_fraction_in_phase, reference_period_start, reference_period_end) VALUES 
-('17acb541-9431-409a-80a8-50eda7e8ebab', 1, '1', 'current', 49348, 0.8399945530060597, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 1, '2', 'current', 8225, 0.1400047661196977, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 1, '3', 'current', 1175, 0.02000068087424253, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 1, '4', 'current', 0, 0, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 1, '5', 'current', 0, 0, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 1, '3+', 'current', 1175, 0.02000068087424253, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 1, 'all', 'current', 58748, 1, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 2, '1', 'first projection', 47586, 0.8100020426227276, '2021-06-01 00:00:00', '2021-08-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 2, '2', 'first projection', 9400, 0.16000544699394023, '2021-06-01 00:00:00', '2021-08-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 4, '3', 'first projection', 1762, 0.0299925103833322, '2021-06-01 00:00:00', '2021-08-31 00:00:00'),
-('17acb541-9431-409a-80a8-50eda7e8ebab', 4, '4', 'first projection', 0, 0, '2021-06-01 00:00:00', '2021-08-31 00:00:00');
+INSERT INTO public.food_security (resource_hdx_id, admin2_ref, provider_admin1_name, provider_admin2_name, ipc_phase, ipc_type, population_in_phase, population_fraction_in_phase, reference_period_start, reference_period_end) VALUES 
+('17acb541-9431-409a-80a8-50eda7e8ebab', 1, 'Provider admin1 name 1','Provider admin2 name 1','1', 'current', 49348, 0.8399945530060597, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 1, 'Provider admin1 name 1','Provider admin2 name 1','2', 'current', 8225, 0.1400047661196977, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 1, 'Provider admin1 name 1','Provider admin2 name 1','3', 'current', 1175, 0.02000068087424253, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 1, 'Provider admin1 name 1','Provider admin2 name 1','4', 'current', 0, 0, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 1, 'Provider admin1 name 1','Provider admin2 name 1','5', 'current', 0, 0, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 1, 'Provider admin1 name 1','Provider admin2 name 1','3+', 'current', 1175, 0.02000068087424253, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 1, 'Provider admin1 name 1','Provider admin2 name 1','all', 'current', 58748, 1, '2021-01-01 00:00:00', '2021-05-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 2, 'Provider admin1 name 2','Provider admin2 name 2','1', 'first projection', 47586, 0.8100020426227276, '2021-06-01 00:00:00', '2021-08-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 2, 'Provider admin1 name 2','Provider admin2 name 2','2', 'first projection', 9400, 0.16000544699394023, '2021-06-01 00:00:00', '2021-08-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 4, 'Provider admin1 name 4','Provider admin2 name 4','3', 'first projection', 1762, 0.0299925103833322, '2021-06-01 00:00:00', '2021-08-31 00:00:00'),
+('17acb541-9431-409a-80a8-50eda7e8ebab', 4, 'Provider admin1 name 4','Provider admin2 name 4','4', 'first projection', 0, 0, '2021-06-01 00:00:00', '2021-08-31 00:00:00');
--- tests/sample_data/poverty_rate.sql
+++ tests/sample_data/poverty_rate.sql
@@ -1,6 +1,6 @@
 -- dummy data
 INSERT INTO poverty_rate(
-	resource_hdx_id, admin1_ref, admin1_name, mpi, headcount_ratio, intensity_of_deprivation, vulnerable_to_poverty, in_severe_poverty, reference_period_start, reference_period_end)
+	resource_hdx_id, admin1_ref, provider_admin1_name, mpi, headcount_ratio, intensity_of_deprivation, vulnerable_to_poverty, in_severe_poverty, reference_period_start, reference_period_end)
 VALUES
 ('17acb541-9431-409a-80a8-50eda7e8ebab',2,'Province 01',0.617442,85.4,72.3,10.5,52.1,'2022-01-01 00:00:00', '2023-01-01 00:00:00'),
 ('17acb541-9431-409a-80a8-50eda7e8ebab',2,'Province 01',0.617442,85.4,72.3,10.5,52.1,'2022-01-02 00:00:00', '2023-01-01 00:00:00'),
