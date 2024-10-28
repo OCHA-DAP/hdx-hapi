b2a14010c137b155a758512ba52a3594bbef021e
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-10-28 13:08:53 +0000

    HDX-10213 Update humanitarian needs sql fixture to include provider names

--- tests/sample_data/humanitarian_needs.sql
+++ tests/sample_data/humanitarian_needs.sql
@@ -1,16 +1,16 @@
 -- dummy data
 INSERT INTO public.humanitarian_needs 
-(resource_hdx_id, admin2_ref, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
-VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab',2, 'All - Disabled - all - AFF','EDU','AFF',500000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
+(resource_hdx_id, admin2_ref, provider_admin1_name, provider_admin2_name, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
+VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab',2, 'provider admin1 name 2','provider admin2 name 2','All - Disabled - all - AFF','EDU','AFF',500000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
 INSERT INTO public.humanitarian_needs 
-(resource_hdx_id, admin2_ref, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
-VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab',4,'Female - child - INN','SHL','INN',1500000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
+(resource_hdx_id, admin2_ref, provider_admin1_name, provider_admin2_name, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
+VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab',4,'provider admin1 name 4','provider admin2 name 4','Female - child - INN','SHL','INN',1500000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
 INSERT INTO public.humanitarian_needs 
-(resource_hdx_id, admin2_ref, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
-VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab', 6,'Male - elderly - all','WSH','all',100000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
+(resource_hdx_id, admin2_ref, provider_admin1_name, provider_admin2_name, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
+VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab', 6,'provider admin1 name 6','provider admin2 name 6','Male - elderly - all','WSH','all',100000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
 INSERT INTO public.humanitarian_needs 
-(resource_hdx_id, admin2_ref, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
-VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab', 4, 'Female - adult - INN','SHL','INN',1500000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
+(resource_hdx_id, admin2_ref, provider_admin1_name, provider_admin2_name, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
+VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab', 4,'provider admin1 name 4','provider admin2 name 4', 'Female - adult - INN','SHL','INN',1500000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
 INSERT INTO public.humanitarian_needs 
-(resource_hdx_id, admin2_ref, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
-VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab', 6,'Male - adult - all','WSH','all',100000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
+(resource_hdx_id, admin2_ref, provider_admin1_name, provider_admin2_name, category, sector_code, population_status, population, reference_period_start, reference_period_end) 
+VALUES ('17acb541-9431-409a-80a8-50eda7e8ebab', 6,'provider admin1 name 6','provider admin2 name 6','Male - adult - all','WSH','all',100000,'2023-01-01 00:00:00', '2025-01-01 00:00:00');
