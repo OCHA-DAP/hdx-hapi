4e8ccab0f0e3e4179117c3513721b4d6dd94eb35
Dan Mihaila <danmihaila@gmail.com>
2025-01-29 10:08:24 +0200

    HDX-10539 - ruff and alembic

--- alembic/env.py
+++ alembic/env.py
@@ -23,13 +23,13 @@ if config.config_file_name is not None:
 from hdx_hapi.db.models.base import Base
 from hapi_schema.db_admin1 import DBAdmin1
 from hapi_schema.db_admin2 import DBAdmin2
-from hapi_schema.db_age_range import DBAgeRange
+# from hapi_schema.db_age_range import DBAgeRange
 from hapi_schema.db_dataset import DBDataset
 from hapi_schema.db_food_security import DBFoodSecurity
-from hapi_schema.db_gender import DBGender
+# from hapi_schema.db_gender import DBGender
 from hapi_schema.db_humanitarian_needs import DBHumanitarianNeeds
-from hapi_schema.db_ipc_phase import DBIpcPhase
-from hapi_schema.db_ipc_type import DBIpcType
+# from hapi_schema.db_ipc_phase import DBIpcPhase
+# from hapi_schema.db_ipc_type import DBIpcType
 from hapi_schema.db_location import DBLocation
 from hapi_schema.db_national_risk import DBNationalRisk
 from hapi_schema.db_operational_presence import DBOperationalPresence
@@ -37,8 +37,8 @@ from hapi_schema.db_org import DBOrg
 from hapi_schema.db_org_type import DBOrgType
 from hapi_schema.db_patch import DBPatch
 from hapi_schema.db_population import DBPopulation
-from hapi_schema.db_population_group import DBPopulationGroup
-from hapi_schema.db_population_status import DBPopulationStatus
+# from hapi_schema.db_population_group import DBPopulationGroup
+# from hapi_schema.db_population_status import DBPopulationStatus
 from hapi_schema.db_resource import DBResource
 from hapi_schema.db_sector import DBSector
 
--- alembic/versions/2d6db74775b5_create_views.py
+++ alembic/versions/2d6db74775b5_create_views.py
@@ -12,21 +12,21 @@ import sqlalchemy as sa
 
 from hapi_schema.db_admin1 import view_params_admin1
 from hapi_schema.db_admin2 import view_params_admin2
-from hapi_schema.db_age_range import view_params_age_range
+# from hapi_schema.db_age_range import view_params_age_range
 from hapi_schema.db_dataset import view_params_dataset
 from hapi_schema.db_food_security import view_params_food_security
-from hapi_schema.db_gender import view_params_gender
+# from hapi_schema.db_gender import view_params_gender
 from hapi_schema.db_humanitarian_needs import view_params_humanitarian_needs
-from hapi_schema.db_ipc_phase import view_params_ipc_phase
-from hapi_schema.db_ipc_type import view_params_ipc_type
+# from hapi_schema.db_ipc_phase import view_params_ipc_phase
+# from hapi_schema.db_ipc_type import view_params_ipc_type
 from hapi_schema.db_location import view_params_location
 from hapi_schema.db_national_risk import view_params_national_risk
 from hapi_schema.db_operational_presence import view_params_operational_presence
 from hapi_schema.db_org import view_params_org
 from hapi_schema.db_org_type import view_params_org_type
 from hapi_schema.db_population import view_params_population
-from hapi_schema.db_population_group import view_params_population_group
-from hapi_schema.db_population_status import view_params_population_status
+# from hapi_schema.db_population_group import view_params_population_group
+# from hapi_schema.db_population_status import view_params_population_status
 from hapi_schema.db_resource import view_params_resource
 from hapi_schema.db_sector import view_params_sector
 
@@ -43,20 +43,20 @@ depends_on: Union[str, Sequence[str], None] = None
 VIEW_LIST = [
     view_params_admin1,
     view_params_admin2,
-    view_params_age_range,
+    # view_params_age_range,
     view_params_dataset,
     view_params_food_security,
-    view_params_gender,
+    # view_params_gender,
     view_params_humanitarian_needs,
-    view_params_ipc_phase,
-    view_params_ipc_type,
+    # view_params_ipc_phase,
+    # view_params_ipc_type,
     view_params_location,
     view_params_national_risk,
     view_params_operational_presence,
     view_params_org_type,
     view_params_org,
-    view_params_population_group,
-    view_params_population_status,
+    # view_params_population_group,
+    # view_params_population_status,
     view_params_population,
     view_params_resource,
     view_params_sector,
